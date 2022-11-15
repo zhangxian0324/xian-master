1d50608be3ac7f8eca61cae3ca263570f82b6855
zhangxian <zhangxian0324@aliyun.com>
2022-11-15 14:29:43 +0800

    一些文件提交

---
+++ xian-common/src/main/java/com/xiancommon/utils/aspect/MyAspect.java
@@ -0,0 +1,17 @@
+package com.xiancommon.utils.aspect;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author zhangxian
+ */
+public class MyAspect {
+    private final static Logger log = LoggerFactory.getLogger("MyAspect.class");
+
+    public void piontCut() {
+
+    }
+
+    
+}
